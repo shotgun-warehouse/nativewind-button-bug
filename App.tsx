b8bb5169baffc7259ce7e766b5b67bf2a60ebf9a
Antoine Rousseau <antoine.rousseau@shotgun.live>
2025-10-02 16:34:56 +0200

    reproduction

--- App.tsx
+++ App.tsx
@@ -1,13 +1,11 @@
-import { ScreenContent } from 'components/ScreenContent';
-import { StatusBar } from 'expo-status-bar';
-
 import './global.css';
+import { Button, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <>
-      <ScreenContent title="Home" path="App.tsx"></ScreenContent>
-      <StatusBar style="auto" />
-    </>
+    <View className='flex-1 items-center justify-center'>
+      <Text>Hello World</Text>
+      <Button title='test' />
+    </View>
   );
 }
