62a1a77ca5e12b0ab004980b7f3fc64007e88964
Antoine Rousseau <antoine.rousseau@shotgun.live>
2025-10-02 23:59:55 +0200

    same with onPress

--- App.tsx
+++ App.tsx
@@ -5,7 +5,7 @@ export default function App() {
   return (
     <View className='flex-1 items-center justify-center'>
       <Text>Hello World</Text>
-      <Button title='test' />
+      <Button title='test' onPress={() => {}} />
     </View>
   );
 }
